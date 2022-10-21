551176b855d4cc3fd9b7dacad0f9baa7fb90f76b
Harsh Parikh <parikh.harsh.hp@gmail.com>
2022-10-21 13:50:15 +0100

    added few lines of code for lab question 2

--- tutorials/lab_2/lab_2_question_2.m
+++ tutorials/lab_2/lab_2_question_2.m
@@ -6,3 +6,12 @@ syms x y;
 % x3 + y = 1
 % y3 − x = −1
 func_1 = [x^3 + y -1; y^3 - x + 1];
+tolerance = 1e-10;
+
+value = solve_equations(func_1, 1, 1);
+
+
+function optimal_solution = solve_equations(functions, guess_x, guess_y)
+    
+    
+end
